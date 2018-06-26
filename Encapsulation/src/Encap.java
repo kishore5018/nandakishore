da603c903b28aac620caa394d42c82f31a798925
HP <HP@192.168.0.2>
2018-06-26 09:57:54 +0530

    hari

---
+++ Encapsulation/src/Encap.java
@@ -0,0 +1,11 @@
+
+public class Encap {
+
+	public static void printdata(encapsulation e) {
+		// TODO Auto-generated method stub
+		System.out.println(e.getId());
+		System.out.println(e.getUsername());
+		System.out.println(e.getPassword());
+	}
+
+}
