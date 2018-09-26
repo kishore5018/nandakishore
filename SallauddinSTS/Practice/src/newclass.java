3f9ad95ac6c45a35f5952c800e7b706b14b113ae
Cubic <Cubic@192.168.0.14>
2018-09-26 16:51:12 +0530

    syso statement in newclass

--- SallauddinSTS/Practice/src/newclass.java
+++ SallauddinSTS/Practice/src/newclass.java
@@ -14,6 +14,7 @@ public class newclass extends classes
 	newclass(int x) {
 		super(x);
 		// TODO Auto-generated constructor stub
+		System.out.println("this is super constructor");
 	}
 
 	public static void main(String[] args) 
